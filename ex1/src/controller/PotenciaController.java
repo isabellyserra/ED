085c601096762e7248167e8c42edc036f6e4fb1a
Isabelly Serra <isabelly.serra@hotmail.com>
2021-08-15 16:56:43 -0300

    add comentários

--- ex1/src/controller/PotenciaController.java
+++ ex1/src/controller/PotenciaController.java
@@ -6,10 +6,12 @@ public class PotenciaController {
 	}
 	
 	public int fatorial(int n) {
-		// Condição de parada
+		// Condição de parada - verifica se o fatorial a ser calculado é zero
 		if (n == 0) {
+			// retorna 1, pois o fatorial de 0 é sempre 1, quebrando assim o loop
 			return 1;
 		} else {
+			// retorna o fatorial recebido multiplicado pelo fatorial -1
 			return n * fatorial(n -1);
 		}
 	}
